df53aaf02c6873332a949e016e5bceef225e109f
sadiqur057 <sadiqur057@gmail.com>
2024-07-13 17:43:39 +0600

    added themewrapper to control theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import { Poppins } from "next/font/google";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins, Arial, sans-serif',
+  },
+});
+
+export default theme;
